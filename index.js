0f241e479d1772beb27c5eec04c94a406c100bcf
Dinesh Singh <dkadesh1999@gmail.com>
2024-05-09 17:47:24 +0530

    this

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3500
+const express = require("express");
+const app = express();
+const port = 3500;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("*/", async (req, resp) => {
+  resp.sendFile(
+    path.join(__dirname, "C:/Users/dkade/Desktop/moneyControll/public/index.html"),
+    function (err) {
+      if (err) {
+        resp.status(500).send(err);
+      }
+    }
+  );
+});
+
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
